29919358d2d36d5807febf5668a0ddc948393ea9
Matthew O <mathonich@yahoo.com>
2023-03-12 17:35:56 -0700

    Created tweet list from generator

--- web_scraper.py
+++ web_scraper.py
@@ -3,7 +3,6 @@ import snscrape.modules.twitter as sn
 import pandas as pd
 import sys
 
-tweet_list = []
 # TODO: numpy list
 # TODO: start and end times for scraping
 def _prompt_search():
@@ -20,7 +19,21 @@ def _print_tweets(tweet_generator):
         if (i > 9):
             break
 
+def _create_tweet_list(n,tweet_generator):
+    tweet_list = []
+    for i,tweet in enumerate(tweet_generator):
+        if (tweet.lang == 'en'):
+            tweet_list.append([tweet.date, tweet.rawContent])
+            n -= 1
+        if n < 1:
+            break
+    return tweet_list
+
+
 if __name__ == "__main__":
     tweet_generator = _prompt_search()
     print(sys.getsizeof(tweet_generator))
     _print_tweets(tweet_generator)
+    tweet_list = _create_tweet_list(int(input("Tweets to store: ")),tweet_generator)
+    sys.getsizeof(tweet_list)
+
