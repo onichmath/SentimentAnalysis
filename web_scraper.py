47f6608853d998a3491527e001d6a4c8f79693d6
Matthew O <mathonich@yahoo.com>
2023-03-13 13:26:35 -0700

    Start and end dates for scraping

--- web_scraper.py
+++ web_scraper.py
@@ -2,13 +2,21 @@
 import snscrape.modules.twitter as sn
 import pandas as pd
 import sys
+import datetime
 
 # TODO: numpy list
 # TODO: start and end times for scraping
 def _prompt_search():
-    search_phrase = input("Phrase to search: ")
-    tweet_generator = sn.TwitterSearchScraper(search_phrase+" since 2023-03-12").get_items()
-    return tweet_generator
+    phrase = input("Phrase to search: ")
+    start_year = input("Start year: ")
+    start_month = input("Start month: ")
+    start_day = input("Start day: ")
+    start_date = f"{start_year}-{start_month}-{start_day}"
+    end_date = f"{datetime.datetime.now():%Y-%m-%d}"
+    search_phrase = f"{phrase} since:{start_date} until:{end_date}"
+    print(search_phrase)
+    #tweet_generator = sn.TwitterSearchScraper(search_phrase).get_items()
+    #return tweet_generator
 
 def _print_tweets(tweet_generator):
     i = int(input("Tweets to print: "))
@@ -16,7 +24,7 @@ def _print_tweets(tweet_generator):
     new_line = '\n'
     for i,tweet in enumerate(tweet_generator):
         if (tweet.lang == 'en'):
-            print(f"{tweet.date} {tweet.rawContent} {new_line}")
+            print(f"{tweet.date} {tweet.renderedContent} {new_line}")
             i-=1
         if (i < 1):
             break
@@ -35,11 +43,13 @@ def _create_tweet_list(tweet_generator):
 
 if __name__ == "__main__":
     tweet_generator = _prompt_search()
-    print(sys.getsizeof(tweet_generator))
+    #print(sys.getsizeof(tweet_generator))
     if input("Print(y/n)?: ") == 'y':
         _print_tweets(tweet_generator)
     if input("Store(y/n)?: ") == 'y':
         tweet_list = _create_tweet_list(tweet_generator)
         tweet_dataframe = pd.DataFrame(tweet_list)
+        # Convertine date time to date for excel compatibility
+        tweet_dataframe['date'] = tweet_dataframe['date'].apply(lambda x: pd.to_datetime(x).date())
         # TODO: relative filepath
         tweet_dataframe.to_excel("/home/matthewo/Documents/Projects/SentimentAnalysis/res/tweet.xlsx")
