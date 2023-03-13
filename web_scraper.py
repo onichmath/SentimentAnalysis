44107d0d77d9eabc960183dd3c7e27f9be70b2ec
Matthew O <mathonich@yahoo.com>
2023-03-12 17:27:34 -0700

    Refactored into functions

--- web_scraper.py
+++ web_scraper.py
@@ -1,13 +1,26 @@
 # TODO: Remove Twitter libraries
 import snscrape.modules.twitter as sn
 import pandas as pd
-i = 0
+import sys
+
+tweet_list = []
 # TODO: numpy list
-search_phrase = input("Phrase to search: ")
-tweet_generator = sn.TwitterSearchScraper(search_phrase).get_items()
-for i,v in enumerate(tweet_generator):
-    if (v.lang == 'en'):
-        print(v.renderedContent)
-        i += 1
-    if (i > 9):
-        break
+# TODO: start and end times for scraping
+def _prompt_search():
+    search_phrase = input("Phrase to search: ")
+    tweet_generator = sn.TwitterSearchScraper(search_phrase).get_items()
+    return tweet_generator
+
+def _print_tweets(tweet_generator):
+    i = 0
+    for i,tweet in enumerate(tweet_generator):
+        if (tweet.lang == 'en'):
+            print(tweet.vibe, tweet.date)
+            i+=1
+        if (i > 9):
+            break
+
+if __name__ == "__main__":
+    tweet_generator = _prompt_search()
+    print(sys.getsizeof(tweet_generator))
+    _print_tweets(tweet_generator)
