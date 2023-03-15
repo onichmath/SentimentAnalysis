200c146ccfd539e3b1af70efa3c47d8b1ac9d58a
Matthew O <mathonich@yahoo.com>
2023-03-15 09:41:01 -0700

    Beginning to implement numpy array for increased performance

--- web_scraper.py
+++ web_scraper.py
@@ -1,15 +1,17 @@
 # TODO: Remove Twitter libraries
 import snscrape.modules.twitter as sn
 import pandas as pd
-import sys
 import datetime
-
+import time
+import numpy as np
+# Before numpy: 55682971337
 # TODO: numpy list
 # TODO: improve space complexity
 # TODO: Store generator instead of dataframe
 def _search_string():
     phrase = str(input("Phrase to search: "))
     start_date = input("Start year: ") +'-'+ input("Start month: ") +'-'+ input("Start day: ")
+    #end_date = input("End year: ") +'-'+ input("End month: ") +'-'+ input("End day: ")
     end_date = f"{datetime.datetime.now():%Y-%m-%d}"
     search_phrase = f"{phrase} since:{start_date} until:{end_date}"
     print(f"Your search phrase is \'{search_phrase}\'")
@@ -25,27 +27,42 @@ def _print_tweets(tweet_generator):
     new_line = '\n'
     for i,tweet in enumerate(tweet_generator):
         if (tweet.lang == 'en'):
-            print(f"{tweet.renderedContent} {new_line}")
+            print(f"{tweet.date}:{tweet.renderedContent} {new_line}")
         if (i >= n):
             break
 
 # TODO: Append date with scrubbed timezone
-def _create_tweet_list(tweet_generator, count):
-    tweet_list = []
-    for i,tweet in enumerate(tweet_generator):
-        if (tweet.lang == 'en'):
-            tweet_list.append([tweet.renderedContent])
-        if i >= count:
-            break
-    return tweet_list
+# TODO: parallel/ improved time 
+def _create_tweet_dataframe(tweet_generator):
+    count = int(input("Tweets to store: "))
+    tweet_dataframe = np.fromiter(tweet_generator, dtype=sn.Tweet, count=count)
+    print(type(tweet_generator))
+    print(type(tweet_dataframe))
+    return tweet_dataframe
+    #tweet_list = []
+    #for i,tweet in enumerate(tweet_generator):
+    #    print(type(tweet))
+    #    if (tweet.lang == 'en'):
+    #        #tweet.date = tweet.date.replace(tzinfo=None)
+    #        tweet_list.append([tweet.date, tweet.renderedContent])
+    #    if i >= count:
+    #        break
+    #tweet_dataframe = pd.DataFrame(data=tweet_list,columns=['Date','Content'])
+    ## Converting Date-Time-Zone to Date-Time
+    #tweet_dataframe['Date'] = pd.to_datetime(tweet_dataframe['Date'])
+    #tweet_dataframe['Date'] = tweet_dataframe['Date'].dt.tz_localize(None)
+    #return tweet_dataframe 
 
+# TODO: Improve space complexity
 if __name__ == "__main__":
     search_phrase = _search_string()
     tweet_generator = _create_generator(search_phrase)
     if input("Print(y/n)?: ") == 'y':
         _print_tweets(tweet_generator)
     if input("Store as excel(y/n)?: ") == 'y':
-        store_count = int(input("Tweets to store: "))
-        tweet_list = _create_tweet_list(tweet_generator,store_count)
-        tweet_dataframe = pd.DataFrame(data=tweet_list,columns=['Content'])
-        tweet_dataframe.to_excel(f"./rsc/{search_phrase}{store_count}.xlsx")
+        start = time.perf_counter_ns()
+        tweet_dataframe = _create_tweet_dataframe(tweet_generator)
+        end = time.perf_counter_ns()
+        print("Time to create dataframe: ",end - start)
+        #tweet_dataframe.to_excel(f"./rsc/{search_phrase}x{store_count}.xlsx")
+        
