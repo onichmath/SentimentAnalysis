035877e42672485eb67fe31ac01393946ba3242d
Matthew O <mathonich@yahoo.com>
2023-03-14 20:59:01 -0700

    Using search string method so excel file is stored as search phrase

--- web_scraper.py
+++ web_scraper.py
@@ -3,20 +3,19 @@ import snscrape.modules.twitter as sn
 import pandas as pd
 import sys
 import datetime
-import pickle
 
-#TODO: deserialize generator to store generator object in file
 # TODO: numpy list
-# TODO: start and end times for scraping
 # TODO: improve space complexity
 # TODO: Store generator instead of dataframe
-def _prompt_search():
+def _search_string():
     phrase = str(input("Phrase to search: "))
     start_date = input("Start year: ") +'-'+ input("Start month: ") +'-'+ input("Start day: ")
     end_date = f"{datetime.datetime.now():%Y-%m-%d}"
     search_phrase = f"{phrase} since:{start_date} until:{end_date}"
     print(f"Your search phrase is \'{search_phrase}\'")
-    # TODO: Fix giant error creating generator
+    return search_phrase
+
+def _prompt_search(search_phrase):
     tweet_generator = sn.TwitterSearchScraper(search_phrase).get_items() 
     return tweet_generator
 
@@ -30,8 +29,7 @@ def _print_tweets(tweet_generator):
         if (i >= n):
             break
 
-
-# TODO: Datetimezone to datetime in createtweet method
+# TODO: Append date with scrubbed timezone
 def _create_tweet_list(tweet_generator):
     tweet_list = []
     n = int(input("Tweets to store: "))
@@ -43,15 +41,11 @@ def _create_tweet_list(tweet_generator):
     return tweet_list
 
 if __name__ == "__main__":
-    tweet_generator = _prompt_search()
-    #print(sys.getsizeof(tweet_generator))
+    search_phrase = _search_string()
+    tweet_generator = _prompt_search(search_phrase)
     if input("Print(y/n)?: ") == 'y':
         _print_tweets(tweet_generator)
-    if input("Store as excel(y/n)?: ") == 'y':
+    if input("Store as excel(y/n)?: ") == 'y': 
         tweet_list = _create_tweet_list(tweet_generator)
         tweet_dataframe = pd.DataFrame(data=tweet_list,columns=['Content'])
-        # Convertine date time to date for excel compatibility
-        #tweet_dataframe['Date'].dt.tz_convert(None)
-        #tweet_dataframe['date'] = tweet_dataframe['date'].apply(lambda x: pd.to_datetime(x).date())
-        # TODO: relative filepath
-        tweet_dataframe.to_excel("/home/matthewo/Documents/Projects/SentimentAnalysis/res/tweet.xlsx")
+        tweet_dataframe.to_excel(f"./rsc/{search_phrase}.xlsx")
