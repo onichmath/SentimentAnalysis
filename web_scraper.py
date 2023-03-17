d846057c5a5a9706b2d3684f0cc18cb6097b9560
Adam Henry <henryada@oregonstate.edu>
2023-03-16 20:32:54 -0700

    add function to get a list of tweets

--- web_scraper.py
+++ web_scraper.py
@@ -1,13 +1,14 @@
-# TODO: Remove Twitter libraries
 import snscrape.modules.twitter as sn
-import pandas as pd
-i = 0
-# TODO: numpy list
-search_phrase = input("Phrase to search: ")
-tweet_generator = sn.TwitterSearchScraper(search_phrase).get_items()
-for i,v in enumerate(tweet_generator):
-    if (v.lang == 'en'):
-        print(v.renderedContent)
-        i += 1
-    if (i > 9):
-        break
+
+def get_tweet_list(phrase: str, count: int) -> list:
+    list_of_tweets = []
+    tweet_generator = sn.TwitterSearchScraper(phrase).get_items()
+    for index, tweet in enumerate(tweet_generator):
+        if (tweet.lang == 'en'):
+            print(f"Tweet by @{tweet.user.username} ({tweet.user.followersCount} followers) with {tweet.likeCount} likes: ", end="")
+            print(tweet.renderedContent.strip("\n"))
+            list_of_tweets.append(tweet.renderedContent.strip("\n"))
+        if (index > count):
+            break
+    return list_of_tweets
+
