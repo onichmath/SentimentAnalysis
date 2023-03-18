7e1de3825f1b7d16cf5a38b7343ff49338d30ba5
Matthew O <mathonich@yahoo.com>
2023-03-18 10:45:10 -0700

    Concated polarity into tweet dataframe. TODO: Visualize data in dataframe

--- data_process.py
+++ data_process.py
@@ -6,14 +6,24 @@ from web_scraper import WebScraper
 class DataProcess:
     def __init__(self):
         self.sent_analyzer = SentimentIntensityAnalyzer()
-        self.polarity_df = self._create_polarity_df_from_dict(WebScraper().tweet_storage)
+        self.tweets_polarity_df = self._create_tweet_polarity_df_from_dict(WebScraper().tweet_storage)
 
     # Converted to df as pandas has easy plotting methods
-    def _create_polarity_df_from_dict(self,tweet_dict):
-        sentiment_dict = {k:[date_tweet[0], date_tweet[1], self.sent_analyzer.polarity_scores(date_tweet[1])]
-                             for k,date_tweet in tweet_dict.items()}
-        polarity_df = pd.DataFrame.from_dict(sentiment_dict, orient='index',columns=['Date','Tweet','Sentiment'])
-        polarity_df['Date'] = pd.to_datetime(polarity_df['Date'])
-        polarity_df['Date'] = polarity_df['Date'].dt.tz_localize(None)
+    def _create_tweet_polarity_df_from_dict(self,tweet_dict):
+        #sentiment_dict = {k:[date_tweet[0], date_tweet[1], self.sent_analyzer.polarity_scores(date_tweet[1])]
+        #                     for k,date_tweet in tweet_dict.items()}
+        tweet_df = pd.DataFrame.from_dict(tweet_dict, orient='index',columns=['Date','Tweet'])
+        tweet_df['Date'] = pd.to_datetime(tweet_df['Date'])
+        tweet_df['Date'] = tweet_df['Date'].dt.tz_localize(None)
+        sentiment_df = self._create_sentiment_df(tweet_dict)
+        polarity_df = pd.concat([tweet_df,sentiment_df], axis=1) 
         return polarity_df
-
+    
+    def _create_sentiment_df(self,tweet_dict):
+        sentiments = []
+        for date_tweet in tweet_dict.values():
+            sentiments.append(self.sent_analyzer.polarity_scores(date_tweet[1]))
+        return pd.DataFrame(sentiments)
+        #return pd.DataFrame.from_dict(self.tweets_polarity_df['Sentiment'], orient='column')
+        #for key,value in self.tweets_polarity_df['Sentiment'].items():
+         #   if key
--- main.py
+++ main.py
@@ -7,14 +7,13 @@ With options for amount of data to scrape
 3. Sentiment analysis on data
 
 4. Display results, options for what to display"""
-from web_scraper import WebScraper
 from data_process import DataProcess
 
 def main():
    #web_scraper = WebScraper()
    #print(web_scraper.tweet_storage)
    polarity = DataProcess()
-   print(polarity.polarity_df)
+   print(polarity.tweets_polarity_df)
 
 if __name__ == "__main__":
         main()
